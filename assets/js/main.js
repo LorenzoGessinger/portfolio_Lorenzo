a8a4fb216f7d6ce94dd34245e705b6cdefa9eeb8
Lorenzo Gessinger <99752428+LorenzoGessinger@users.noreply.github.com>
2023-01-06 17:46:08 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const btnMobile = document.getElementById('btn-mobile')
+
+const toggleMenu = (event) => {
+    if(event.type === 'touchstart') event.preventDefault()
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active') // add case it has, does not add case do not have.
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
