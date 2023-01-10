0a1b905920e217e499e13fa2c8921c01e249ae19
Lorenzo Gessinger <99752428+LorenzoGessinger@users.noreply.github.com>
2023-01-10 15:38:52 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
 const btnMobile = document.getElementById('btn-mobile')
+const nav = document.getElementById('nav')
 
 const toggleMenu = (event) => {
-    if(event.type === 'touchstart') event.preventDefault()
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav')
     nav.classList.toggle('active') // add case it has, does not add case do not have.
     const active = nav.classList.contains('active')
     event.currentTarget.setAttribute('aria-expanded', active)
-    if(active) {
+    if (active) {
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
     } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
     }
 }
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    nav.classList.remove('active')
+}))
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
